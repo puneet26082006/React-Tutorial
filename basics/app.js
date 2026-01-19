80c1d4dc55e4a3e618a686aea7ef450921b0c7a0
Puneet Saxena <puneetsaxena168@gmail.com>
2026-01-19 18:33:56 +0530

    01-folder and Basic folder

---
+++ basics/app.js
@@ -0,0 +1,5 @@
+const arr = [1,2,3,4,5];
+
+export const user = "Puneet";
+
+export default arr ;
---
+++ basics/script.js
@@ -0,0 +1,12 @@
+// var h1 = document.createElement("h1")
+// h1.innerHTML = 'Hello from Js';
+
+// document.body.appendChild(h1);
+// console.log(h1);
+
+
+import array from "./app.js"     // default import 
+import { user } from "./app.js"  // named import 
+
+console.log(array);
+console.log(user);
